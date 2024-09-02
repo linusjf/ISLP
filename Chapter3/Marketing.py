362f691e6582bb73b2151d67411453ef8a21b4ef
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-02 20:04:21 +0530

    feat: Marketing exercise

--- Chapter3/Marketing.py
+++ Chapter3/Marketing.py
@@ -32,6 +32,7 @@ import statsmodels.api as sm
 
 # %%
 from statsmodels.stats.outliers_influence import variance_inflation_factor as VIF
+from statsmodels.stats.outliers_influence import summary_table
 from statsmodels.stats.anova import anova_lm
 
 # %% [markdown]
@@ -111,4 +112,58 @@ results.resid.std()
 # %% [markdown]
 # The low p-values for Radio and TV suggest that only they are related to Sales.
 
+# %% [markdown]
+# ## How large is the association between each medium and sales?
+
 # %%
+results.conf_int(alpha=0.05)
+
+# %% [markdown]
+# The confidence intervals for TV and Radio are narrow and far from zero. This provides evidence that these media are related to sales.
+#
+#
+
+# %% [markdown]
+# The interval for Newspaper includes zero indicating that it is not statistically significant given values of TV and Radio.
+
+# %%
+vals = [VIF(X,i) for i in range(1, X.shape[1])]
+print(vals)
+
+# %% [markdown]
+# The VIF scores are 1.005, 1.145 and 1.145 respectively for TV, radio and newspaper. These suggest no evidence of collinearity as an explnation for wide standard errors for newspaper.
+
+# %% [markdown]
+# In order to assess the association of each medium individually on sales, we can perform three separate linear regressions.
+
+# %%
+TV = Advertising["TV"]
+Radio = Advertising["Radio"]
+Newspaper = Advertising["Newspaper"]
+
+
+# %%
+TV = MS(["TV"]).fit_transform(Advertising)
+model = sm.OLS(y, TV)
+results = model.fit()
+print(summarize(results))
+Radio = MS(["Radio"]).fit_transform(Advertising)
+model = sm.OLS(y, Radio)
+results = model.fit()
+print(summarize(results))
+Newspaper = MS(["Newspaper"]).fit_transform(Advertising)
+model = sm.OLS(y, Newspaper)
+results = model.fit()
+print(summarize(results))
+
+# %% [markdown]
+# Looking at the p-values, there is evidence of a strong association b/w TV and sales and Radio and sales. There is evidence of a mild association between Newspaper and sales when TV and radio are ignored.
+
+# %%
+## How accurately can we predict future sales?
+
+# %%
+## Is the relationship linear?
+
+# %%
+## Is there synergy among the advertising media?
