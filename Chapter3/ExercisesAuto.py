c3199f490e6802e7ccb6754c8e1aa1a839139a7f
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-04 11:47:42 +0530

    fix: update genipynb to look for files, not directories feat: start on exercises

---
+++ Chapter3/ExercisesAuto.py
@@ -0,0 +1,45 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.4
+#   kernelspec:
+#     display_name: islpenv
+#     language: python
+#     name: islpenv
+# ---
+
+# %% [markdown]
+# Import standard libraries
+
+# %%
+import numpy as np
+import pandas as pd
+from matplotlib.pyplot import subplots
+#import seaborn as sns
+
+# %% [markdown]
+# New imports
+
+# %%
+import statsmodels.api as sm
+
+# %% [markdown]
+# Import statsmodel.objects
+
+# %%
+from statsmodels.stats.outliers_influence import variance_inflation_factor as VIF
+from statsmodels.stats.outliers_influence import summary_table
+from statsmodels.stats.anova import anova_lm
+
+# %% [markdown]
+# Import ISLP objects
+
+# %%
+import ISLP
+from ISLP import models
+from ISLP import load_data
+from ISLP.models import (ModelSpec as MS, summarize, poly)
