c22be5d3f8922263a865645f0bf2be478ead5d74
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-04 13:49:30 +0530

    feat: start on exercises

--- Chapter3/ExercisesAuto.py
+++ Chapter3/ExercisesAuto.py
@@ -99,7 +99,42 @@ model = sm.OLS(y, X)
 results = model.fit()
 summarize(results)
 
+# %% [raw]
+# - There is evidence of a linear relationship between horespower and the response mpg.
+
 # %%
 results.summary()
+
+# %% [markdown]
+# + The R<sup>2</sup> value of 60.6% indicates that the regression of horsepower on mpg explains 60.6% of the variation in the model.
+
+# %% [markdown]
+# + The relationship between horsepower and  mpg is negative, i.e., an increase in hp of 1 unit decreases the mileage by 0.1578 miles. An increase in the car's output in power is offset by a decrease in its economy.
+
+# %%
+design = MS(["horsepower"])
+new_df = pd.DataFrame({"horsepower": [98]})
+design = design.fit(new_df)
+newX = design.transform(new_df)
+
+# %%
+new_predictions = results.get_prediction(newX)
+mileage = new_predictions.predicted_mean[0]
+mileage
+
+# %% [markdown]
+# - The predicted mileage for a horsepower of 98 is 24.47 mpg.
+
+# %%
+new_predictions.conf_int(alpha=0.05)
+
+# %% [markdown]
+# + The 95% confidence interval is (23.97, 24.96)
+
+# %%
+new_predictions.conf_int(alpha=0.05, obs=True)
+
+# %% [markdown]
+# + The 95% prediction interval is (14.82, 34.13)
 
 # %%
