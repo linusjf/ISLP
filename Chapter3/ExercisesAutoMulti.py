effa40ad065580916ea8600525cb8827fc9dadcf
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-05 18:47:48 +0530

    feat: continue with multilinear regression for Auto dataset

--- Chapter3/ExercisesAutoMulti.py
+++ Chapter3/ExercisesAutoMulti.py
@@ -37,6 +37,7 @@ import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor as VIF
 from statsmodels.stats.outliers_influence import summary_table
 from statsmodels.stats.anova import anova_lm
+import statsmodels.formula.api as smf
 
 # %% [markdown]
 # ## Import ISLP objects
@@ -58,14 +59,6 @@ Auto.shape
 Auto.describe()
 
 # %% [markdown]
-# ## Convert cylinders and origin columns to categorical types
-
-# %%
-Auto["cylinders"] = Auto["cylinders"].astype("category")
-Auto["origin"] = Auto["origin"].astype("category")
-Auto.describe()
-
-# %% [markdown]
 # ## 9. This question involves the use of multiple linear regression on the Auto data set.
 
 # %% [markdown]
@@ -76,5 +69,36 @@ pd.plotting.scatter_matrix(Auto, figsize = (14, 14));
 
 # %%
 Auto.corr()
+
+# %% [markdown]
+# ## Convert cylinders and origin columns to categorical types
+
+# %%
+Auto["cylinders"] = Auto["cylinders"].astype("category")
+Auto["origin"] = Auto["origin"].astype("category")
+Auto.describe()
+
+# %% [markdown]
+# ### Encode categorical variables as dummy variables dropping the first to remove multicollinearity.
+
+# %%
+Auto = pd.get_dummies(Auto, columns=list(["cylinders", "origin"]), drop_first = True, dtype = np.uint8)
+Auto.columns
+
+# %%
+y = Auto["mpg"]
+Auto.columns.drop("mpg")
+
+# %%
+cols = list(Auto.columns)
+cols.remove("mpg")
+X = MS(cols).fit_transform(Auto)
+formula = ' + '.join(cols)
+model = smf.ols(f'mpg ~ {formula}', data=Auto)
+results = model.fit()
+summarize(results)
+
+# %%
+anova_lm(results)
 
 # %%
