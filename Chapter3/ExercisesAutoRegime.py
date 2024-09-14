c7234f5aa37c9b31a9bf9b903f0fecb0c6fd2a60
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-14 18:16:51 +0530

    feat: perform two separate regressions for Auto dataset

--- Chapter3/ExercisesAutoRegime.py
+++ Chapter3/ExercisesAutoRegime.py
@@ -13,10 +13,13 @@
 # ---
 
 # %% [markdown]
-# # Auto datsset two regimes: Pre-oilshock and Post-oilshock
+# # Auto dataset two regimes: Pre-oilshock and Post-oilshock
 
 # %% [markdown]
 # ## We can also test if there are two regimes that contribute to the heteroskedasticity by running separate regressions for pre-oilshock and post-oilshock.
+
+# %% [markdown] jp-MarkdownHeadingCollapsed=true
+# ### Set up IPython libraries for customizing notebook display
 
 # %%
 from IPython.core.interactiveshell import InteractiveShell
@@ -32,7 +35,7 @@ def allDone():
 
 
 # %% [markdown]
-# ## Import standard libraries
+# ### Import standard libraries
 
 # %%
 import numpy as np
@@ -47,12 +50,12 @@ import seaborn as sns
 import itertools
 
 # %% [markdown]
-# ## Statsmodels imports
+# ### Statsmodels imports
 
 # %%
 import statsmodels.api as sm
 
-# %% [markdown]
+# %% [markdown] jp-MarkdownHeadingCollapsed=true
 # ### Import statsmodels.objects
 
 # %%
@@ -61,8 +64,8 @@ from statsmodels.stats.outliers_influence import summary_table
 from statsmodels.stats.anova import anova_lm
 import statsmodels.formula.api as smf
 
-# %% [markdown]
-# ## Import ISLP objects
+# %% [markdown] jp-MarkdownHeadingCollapsed=true
+# ### Import ISLP objects
 
 # %%
 import ISLP
@@ -79,11 +82,12 @@ Auto = Auto.dropna()
 Auto.shape
 Auto.describe()
 
-# %% [markdown]
-# ## Convert year and origin columns to categorical types
+# %% [markdown] jp-MarkdownHeadingCollapsed=true
+# ### Convert year and origin columns to categorical types
 
 # %%
 Auto["origin"] = Auto["origin"].astype("category")
+Auto['origin'] = Auto['origin'].cat.rename_categories({1:'America', 2:'Europe', 3:'Japan'})
 Auto.describe()
 
 # %%
@@ -93,6 +97,7 @@ Auto.describe()
 Auto_preos = Auto[Auto["year"] <= 76]
 Auto_preos.shape
 Auto_preos.describe()
+Auto_preos.corr(numeric_only=True)
 
 # %%
 Auto_postos = Auto[Auto["year"] > 76]
@@ -103,6 +108,64 @@ Auto_postos.describe()
 display("If you look at the two datasets as displayed above, it's evident that the oil shock had a major impact on the models produced since.")
 display(Auto_preos.mean(numeric_only=True), Auto_postos.mean(numeric_only=True))
 display("Mileage increased, number of cylinders decreased, displacement decreased, horsepower decreased, weight decreased and time to acceleration increased thus indicating that less powerful and less performant cars were produced in the immediate period after the oil shock of 1973.")
+
+# %% [markdown]
+# ### Encode categorical variables as dummy variables dropping the first to remove multicollinearity.
+
+# %%
+Auto_preos = pd.get_dummies(Auto_preos, columns=list(["origin"]), drop_first = True, dtype = np.uint8)
+#Auto_preos = Auto_preos.drop(columns=["cylinders","displacement","acceleration"])
+Auto_preos.columns
+Auto_postos = pd.get_dummies(Auto_postos, columns=list(["origin"]), drop_first = True, dtype = np.uint8)
+#Auto_postos = Auto_postos.drop(columns=["cylinders","displacement","acceleration"])
+Auto_postos.columns
+
+# %% [markdown]
+# ### Run linear regression for pre Oil Shock
+
+# %%
+cols = list(Auto_preos.columns)
+cols.remove("mpg")
+formula = ' + '.join(cols)
+model = smf.ols(f'mpg ~ {formula}', data=Auto_preos)
+results = model.fit()
+results.summary()
+anova_lm(results)
+
+# %% [markdown]
+# ### Residual plot for pre oil shock
+
+# %%
+_, ax = subplots(figsize=(8,8))
+ax.scatter(results.fittedvalues, results.resid)
+ax.set_xlabel("Fitted values for mpg")
+ax.set_ylabel("Residuals")
+ax.axhline(0, c="k", ls="--");
+
+# %%
+cols = list(Auto_postos.columns)
+cols.remove("mpg")
+formula = ' + '.join(cols)
+model = smf.ols(f'mpg ~ {formula}', data=Auto_postos)
+results = model.fit()
+results.summary()
+anova_lm(results)
+
+# %% [markdown]
+# ### Run linear regression for post Oil Shock
+
+# %% [markdown]
+# ### Residual plot for post oil shock
+
+# %%
+_, ax = subplots(figsize=(8,8))
+ax.scatter(results.fittedvalues, results.resid)
+ax.set_xlabel("Fitted values for mpg")
+ax.set_ylabel("Residuals")
+ax.axhline(0, c="k", ls="--");
+
+# %% [markdown]
+# ## Finished
 
 # %%
 allDone()
