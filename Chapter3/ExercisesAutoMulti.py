63d05a823e3c7c18e434b550c8a1bb7683a7919c
Linus Fernandes <linus.fernandes@gmail.com>
2024-09-25 05:21:25 +0530

    fix: update AutoMulti

--- Chapter3/ExercisesAutoMulti.py
+++ Chapter3/ExercisesAutoMulti.py
@@ -27,6 +27,7 @@ from notebookfuncs import *
 # %%
 import numpy as np
 import pandas as pd
+
 pd.set_option('display.max_rows', 1000)
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.width', 1000)
@@ -70,7 +71,7 @@ from userfuncs import *
 # #### Set level of significance (alpha)
 
 # %%
-LOS_Alpha = 0.01;
+LOS_Alpha = 0.01
 
 # %%
 Auto = load_data('Auto')
@@ -91,7 +92,7 @@ Auto.describe()
 # ### (a) Produce a scatterplot matrix which includes all of the variables in the data set.
 
 # %%
-pd.plotting.scatter_matrix(Auto, figsize = (14, 14));
+pd.plotting.scatter_matrix(Auto, figsize=(14, 14))
 
 # %% [markdown]
 # ### (b) Compute the matrix of correlations between the variables using the DataFrame.corr() method.
@@ -107,19 +108,36 @@ Auto.corr()
 
 # %%
 Auto["origin"] = Auto["origin"].astype("category")
-Auto['origin'] = Auto['origin'].cat.rename_categories({1:'America', 2:'Europe', 3:'Japan'})
+Auto['origin'] = Auto['origin'].cat.rename_categories({
+    1: 'America',
+    2: 'Europe',
+    3: 'Japan'
+})
 Auto["year"] = Auto["year"].astype("category")
 Auto.describe()
 
 # %%
-sns.relplot(Auto, x="year", y="weight", col="origin", hue="cylinders", style="cylinders", estimator='mean', kind="line");
+sns.relplot(Auto,
+            x="year",
+            y="weight",
+            col="origin",
+            hue="cylinders",
+            style="cylinders",
+            estimator='mean',
+            kind="line")
 
 # %% [markdown]
 # #### The weight of the 8-cylinder American made models show a decline from the highs of 1972. It can also be seen that American made cars are heavier than their European and Japanese counterparts especially in the most common models with 4 cylinders.
 
 # %%
-sns.relplot(Auto, x="year", y="mpg", col="origin", hue="cylinders", style="cylinders", estimator='mean', kind="line");
-
+sns.relplot(Auto,
+            x="year",
+            y="mpg",
+            col="origin",
+            hue="cylinders",
+            style="cylinders",
+            estimator='mean',
+            kind="line")
 
 # %% [markdown]
 # #### It can be seen that after the [oil shock of 1973](https://en.wikipedia.org/wiki/1973_oil_crisis) and the regulations and actions taken by the US government, the mileage for American made cars rose across all models. This was, however, matched by the European and Japanese models which were already lighter and more fuel efficient.
@@ -127,20 +145,22 @@ sns.relplot(Auto, x="year", y="mpg", col="origin", hue="cylinders", style="cylin
 # %% [markdown]
 # ### Encode categorical variables as dummy variables dropping the first to remove multicollinearity.
 
+
 # %%
 def categorize_for_oil_shock(row):
-  # we add 3 years because it takes approximately that long for car manufacturers to introduce a new model
-  if row["year"] in (70, 71, 72, 73, 74, 75, 76):
-    return 0;
-  return 1;
+    # we add 3 years because it takes approximately that long for car manufacturers to introduce a new model
+    if row["year"] in (70, 71, 72, 73, 74, 75, 76):
+        return 0
+    return 1
 
-Auto["oilshock"] = Auto.apply(categorize_for_oil_shock, axis=1);
 
-# %%
-Auto.boxplot(column="mpg", by=["oilshock", "origin"]);
+Auto["oilshock"] = Auto.apply(categorize_for_oil_shock, axis=1)
 
 # %%
-Auto_os = Auto.drop(["year"], axis = 1)
+Auto.boxplot(column="mpg", by=["oilshock", "origin"])
+
+# %%
+Auto_os = Auto.drop(["year"], axis=1)
 Auto_os.columns
 
 # %%
@@ -150,11 +170,14 @@ Auto_os = Auto_os.apply(standardize)
 Auto_os.describe()
 
 # %%
-Auto_os = pd.get_dummies(Auto_os, columns=list(["origin"]), drop_first = True, dtype = np.uint8)
+Auto_os = pd.get_dummies(Auto_os,
+                         columns=list(["origin"]),
+                         drop_first=True,
+                         dtype=np.uint8)
 Auto_os.columns
 
 # %%
-y = Auto_os["mpg"];
+y = Auto_os["mpg"]
 
 # %%
 cols = list(Auto_os.columns)
@@ -166,15 +189,15 @@ results.summary()
 
 # %% [markdown]
 # #### i. Is there a relationship between the predictors and the response? Use the anova_lm() function from statsmodels to answer this question.
-# #### ii. Which predictors appear to have a statistically significant relationship to the response? 
+# #### ii. Which predictors appear to have a statistically significant relationship to the response?
 # #### iii. What does the coefficient for the year variable suggest?
 
 # %%
 anova_lm(results)
 
 # %% [markdown]
-# #### There seems to be a statistical relationship between all of the predictors and the response variable, mpg, except for acceleration. 
-# #### Even though some of the categorical variables are insignificant, even if one of the levels is significant, it is advisable to retain them all in the model. 
+# #### There seems to be a statistical relationship between all of the predictors and the response variable, mpg, except for acceleration.
+# #### Even though some of the categorical variables are insignificant, even if one of the levels is significant, it is advisable to retain them all in the model.
 #
 # <https://stats.stackexchange.com/questions/24298/can-i-ignore-coefficients-for-non-significant-levels-of-factors-in-a-linear-mode>
 #
@@ -190,14 +213,16 @@ anova_lm(results)
 Auto_os.corr(numeric_only=True)
 
 # %%
-vifdf = calculate_VIFs("mpg ~ " + " + ".join(Auto_os.columns) + " - mpg", Auto_os)
+vifdf = calculate_VIFs("mpg ~ " + " + ".join(Auto_os.columns) + " - mpg",
+                       Auto_os)
 vifdf
 
 # %%
 identify_highest_VIF_feature(vifdf)
 
 # %%
-vifdf = calculate_VIFs("mpg ~ " + " + ".join(Auto_os.columns) + " - mpg - displacement", Auto_os)
+vifdf = calculate_VIFs(
+    "mpg ~ " + " + ".join(Auto_os.columns) + " - mpg - displacement", Auto_os)
 vifdf
 
 # %%
@@ -211,7 +236,7 @@ cols = list(Auto_os.columns)
 cols.remove("mpg")
 cols.remove("displacement")
 formula = ' + '.join(cols)
-results = perform_analysis("mpg",formula,Auto_os);
+results = perform_analysis("mpg", formula, Auto_os)
 
 # %%
 identify_least_significant_feature(results, alpha=LOS_Alpha)
@@ -222,10 +247,14 @@ identify_least_significant_feature(results, alpha=LOS_Alpha)
 # %%
 cols.remove("acceleration")
 formula = ' + '.join(cols)
-results = perform_analysis("mpg",formula,Auto_os);
+results = perform_analysis("mpg", formula, Auto_os)
 simple_model = results
 models = []
-models.append({"name": "simple_model", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+models.append({
+    "name": "simple_model",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %% [markdown]
 # #### Linear Regression after dropping cylinders. The model now is mpg ~  horsepower + weight + oilshock +  origin_Europe + origin_Japan
@@ -233,22 +262,26 @@ models.append({"name": "simple_model", "model" : results.model.formula, "R-squar
 # %%
 cols.remove("cylinders")
 formula = ' + '.join(cols)
-results = perform_analysis("mpg",formula,Auto_os);
+results = perform_analysis("mpg", formula, Auto_os)
 simple_model = results
 models = []
-models.append({"name": "simple_model", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+models.append({
+    "name": "simple_model",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %% [markdown]
 # #### We can now try and plot the diagnostics for the model.
 
 # %%
-TSS = np.sum((y - np.mean(y)) ** 2)
+TSS = np.sum((y - np.mean(y))**2)
 TSS
-RSS = np.sum((y - results.fittedvalues) ** 2)
+RSS = np.sum((y - results.fittedvalues)**2)
 RSS
-RSE = np.sqrt(RSS/results.df_model)
+RSE = np.sqrt(RSS / results.df_model)
 display("RSE " + str(RSE))
-display("R-squared adjusted : " + str(results.rsquared_adj)) 
+display("R-squared adjusted : " + str(results.rsquared_adj))
 display("F-statistic : " + str(results.fvalue))
 
 # %%
@@ -263,18 +296,26 @@ display_residuals_plot(results)
 # %%
 formula = ' + '.join(cols)
 formula += " + " + "horsepower: weight"
-results = perform_analysis("mpg", formula, Auto_os);
+results = perform_analysis("mpg", formula, Auto_os)
 numeric_interactions = results
-models.append({"name": "numeric_interactions", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+models.append({
+    "name": "numeric_interactions",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %%
 formula = ' + '.join(cols)
 formula += " + " + "horsepower: weight"
 formula += " + " + "oilshock: weight"
 formula += " + " + "oilshock: horsepower"
-results = perform_analysis("mpg", formula, Auto_os);
-oilshock_interactions = results;
-models.append({"name": "oilshock_interactions", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+results = perform_analysis("mpg", formula, Auto_os)
+oilshock_interactions = results
+models.append({
+    "name": "oilshock_interactions",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %%
 formula = ' + '.join(cols)
@@ -285,8 +326,8 @@ formula += " + " + "origin_Europe: weight"
 formula += " + " + "origin_Japan: weight"
 formula += " + " + "oilshock: weight"
 formula += " + " + "oilshock: horsepower"
-results = perform_analysis("mpg", formula, Auto_os);
-origin_interactions = results;
+results = perform_analysis("mpg", formula, Auto_os)
+origin_interactions = results
 
 # %% [markdown]
 # + From the above analysis, we can see that there is no significant interaction between origin and weight.
@@ -299,8 +340,8 @@ formula += " + " + "origin_Europe: horsepower"
 formula += " + " + "origin_Japan: horsepower"
 formula += " + " + "oilshock: weight"
 formula += " + " + "oilshock: horsepower"
-results = perform_analysis("mpg", formula, Auto_os);
-origin_interactions = results;
+results = perform_analysis("mpg", formula, Auto_os)
+origin_interactions = results
 
 # %% [markdown]
 # + From the above analysis, it is evident that with the interaction between origin and horsepower, the interaction between oilshock and weight and horsepower is insignificant. We can drop these from the model as well.
@@ -310,15 +351,20 @@ formula = ' + '.join(cols)
 formula += " + " + "oilshock: horsepower"
 formula += " + " + "origin_Europe: horsepower"
 formula += " + " + "origin_Japan: horsepower"
-results = perform_analysis("mpg", formula, Auto_os);
-origininteractions = results;
-models.append({"name": "origin_interactions", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+results = perform_analysis("mpg", formula, Auto_os)
+origin_interactions = results
+models.append({
+    "name": "origin_interactions",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %%
 display_residuals_plot(results)
 
 # %%
-anova_lm(simple_model,numeric_interactions, oilshock_interactions, origin_interactions)
+anova_lm(simple_model, numeric_interactions, oilshock_interactions,
+         origin_interactions)
 
 # %%
 pd.DataFrame(models)
@@ -328,19 +374,23 @@ pd.DataFrame(models)
 
 # %%
 formula = simple_model.model.formula
-formula =formula[formula.rindex("~") + 1:]
+formula = formula[formula.rindex("~") + 1:]
 # Add higher order transformations for horsepower and weight
 formula += " + " + "I(horsepower**2)"
 formula += " + " + "I(weight**2)"
 results = perform_analysis("mpg", formula, Auto_os)
 squared_transformations = results
-models.append({"name": "squared_transformation", "model" : results.model.formula, "R-squared adjusted" : results.rsquared_adj})
+models.append({
+    "name": "squared_transformation",
+    "model": results.model.formula,
+    "R-squared adjusted": results.rsquared_adj
+})
 
 # %%
 display_residuals_plot(results)
 
 # %%
-anova_lm( simple_model, squared_transformations )
+anova_lm(simple_model, squared_transformations)
 
 # %% [markdown]
 # - Since we've standardized the variables, we cannot run log or square root transformations on the negative valued columns.
