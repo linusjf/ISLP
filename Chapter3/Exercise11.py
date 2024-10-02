27b7dcdf9f4fa2a802479f6bcf3149fc7cc1592c
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-02 10:08:19 +0000

    feat: Use Latex MathJax in Exercise 11 notebook

--- Chapter3/Exercise11.py
+++ Chapter3/Exercise11.py
@@ -13,7 +13,7 @@
 # ---
 
 # %% [markdown]
-# # 11. In this problem we will investigate the t-statistic for the null hypothesis H0 : β = 0 in simple linear regression without an intercept. To begin, we generate a predictor x and a response y as follows.
+# # 11) In this problem we will investigate the t-statistic for the null hypothesis H0 : β = 0 in simple linear regression without an intercept. To begin, we generate a predictor x and a response y as follows.
 
 # %% [markdown]
 # ```python
@@ -32,13 +32,24 @@
 # ## (c) What is the relationship between the results obtained in (a) and (b)?
 
 # %% [markdown]
-# ## (d) For the regression of Y onto X without an intercept, the t-statistic for H0 : β = 0 takes the form β̂/SE(β̂), where β̂ is given by (3.38), and where 
-# SE(β̂) = 
+# ## (d) For the regression of Y onto X without an intercept, the t-statistic for H0 : β = 0 takes the form $\hat{\beta} / SE(\hat{\beta})$, where $\hat{\beta}$ is given by (3.38), and where 
 
 # %% [markdown]
 # $$
-# f(x) = \int_{-\infty}^{\infty} e^{-x^2} dx
+# \Large SE(\hat{\beta}) = \sqrt{\frac {\sum_{i=1}^{n}(y_{i} - x_{i}\hat{\beta})^{2}} {(n - 1)\sum_{i^{'}=1}^n x_{i^{'}}^2}}
 # $$
 #
 
-# %%
+# %% [markdown]
+# ## (These formulas are slightly different from those given in Sections 3.1.1 and 3.1.2, since here we are performing regression without an intercept.) Show algebraically, and confirm numerically in Python, that the t-statistic can be written as
+
+# %% [markdown]
+# $$
+# \Large \frac {(n - 1) \sum_{i=1}^n x_{i}y_{i}} {\sqrt {(\sum_{i=1}^n x_{i}^2)(\sum_{i=1}^n y_{i}^2) - (\sum_{i^{'}=1}^{n}x_{i^{'}}y_{i^{'}})^{2}}}
+# $$
+
+# %% [markdown]
+# ## (e) Using the results from (d), argue that the t-statistic for the regression of y onto x is the same as the t-statistic for the regression of x onto y. 
+
+# %% [markdown]
+# ## (f) In Python, show that when regression is performed with an intercept, the t-statistic for H0 : β1 = 0 is the same for the regression of y onto x as it is for the regression of x onto y.
