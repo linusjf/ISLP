9bdafea04515cc91ecd264127cf7f721aefe2ae1
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-02 14:26:42 +0000

    feat: Use Latex MathJax in Exercise 11 notebook

--- Chapter3/Exercise11.py
+++ Chapter3/Exercise11.py
@@ -13,7 +13,7 @@
 # ---
 
 # %% [markdown]
-# # 11) In this problem we will investigate the t-statistic for the null hypothesis H0 : $\beta$ = 0 in simple linear regression without an intercept. To begin, we generate a predictor x and a response y as follows.
+# # 11) In this problem we will investigate the t-statistic for the null hypothesis $H_{0} : \beta = 0$ in simple linear regression without an intercept. To begin, we generate a predictor x and a response y as follows.
 
 # %% [markdown]
 # ```python
@@ -23,20 +23,20 @@
 # ```
 
 # %% [markdown]
-# ## (a) Perform a simple linear regression of y onto x, without an intercept. Report the coefficient estimate $\hat{\beta}$ the standard error of this coefficient estimate, and the t-statistic and p-value associated with the null hypothesis H0 : $\beta$ = 0. Comment on these results. (You can perform regression without an intercept using the keywords argument intercept=False to ModelSpec().)
+# ## (a) Perform a simple linear regression of y onto x, without an intercept. Report the coefficient estimate $\hat{\beta}$ the standard error of this coefficient estimate, and the t-statistic and p-value associated with the null hypothesis $H_{0} : \beta = 0$. Comment on these results. (You can perform regression without an intercept using the keywords argument intercept=False to ModelSpec().)
 
 # %% [markdown]
-# ## (b) Now perform a simple linear regression of x onto y without an intercept, and report the coefficient estimate, its standard error, and the corresponding t-statistic and p-values associated with the null hypothesis H0 : $\beta$ = 0. Comment on these results.
+# ## (b) Now perform a simple linear regression of x onto y without an intercept, and report the coefficient estimate, its standard error, and the corresponding t-statistic and p-values associated with the null hypothesis $H_{0} : \beta = 0$. Comment on these results.
 
 # %% [markdown]
 # ## (c) What is the relationship between the results obtained in (a) and (b)?
 
 # %% [markdown]
-# ## (d) For the regression of Y onto X without an intercept, the t-statistic for H0 : β = 0 takes the form $\hat{\beta} / SE(\hat{\beta})$, where $\hat{\beta}$ is given by (3.38), and where 
+# ## (d) For the regression of Y onto X without an intercept, the t-statistic for $H_{0} : \beta = 0$ takes the form $\hat{\beta} / SE(\hat{\beta})$, where $\hat{\beta}$ is given by (3.38), and where
 
 # %% [markdown]
 # $$
-# \Large SE(\hat{\beta}) = \sqrt{\frac {\sum_{i=1}^{n}(y_{i} - x_{i}\hat{\beta})^{2}} {(n - 1)\sum_{i^{'}=1}^n x_{i^{'}}^2}}
+# \Large SE(\hat{\beta}) = \sqrt{\frac {\sum_{i=1}^{n} \left (y_{i} - x_{i}\hat{\beta} \right )^{2}} {\left (n - 1 \right )\sum_{i^{'}=1}^n x_{i^{'}}^2}}
 # $$
 #
 
@@ -45,11 +45,13 @@
 
 # %% [markdown]
 # $$
-# \Large \frac {(n - 1) \sum_{i=1}^n x_{i}y_{i}} {\sqrt {(\sum_{i=1}^n x_{i}^2)(\sum_{i=1}^n y_{i}^2) - (\sum_{i^{'}=1}^{n}x_{i^{'}}y_{i^{'}})^{2}}}
+# \Large \frac {\left (n - 1 \right ) \sum_{i=1}^n x_{i}y_{i}} {\sqrt {\left (\sum_{i=1}^n x_{i}^2 \right ) \left (\sum_{i=1}^n y_{i}^2 \right ) - \left (\sum_{i^{'}=1}^{n}x_{i^{'}}y_{i^{'}} \right )^{2}}}
 # $$
 
 # %% [markdown]
-# ## (e) Using the results from (d), argue that the t-statistic for the regression of y onto x is the same as the t-statistic for the regression of x onto y. 
+# ## (e) Using the results from (d), argue that the t-statistic for the regression of y onto x is the same as the t-statistic for the regression of x onto y.
 
 # %% [markdown]
-# ## (f) In Python, show that when regression is performed with an intercept, the t-statistic for H0 : $\beta$1 = 0 is the same for the regression of y onto x as it is for the regression of x onto y.
+# ## (f) In Python, show that when regression is performed with an intercept, the t-statistic for $H_{0} : \beta_{1} = 0$ is the same for the regression of y onto x as it is for the regression of x onto y.
+
+# %%
