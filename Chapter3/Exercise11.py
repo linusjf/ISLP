50fed1a7537c319ee82de40d453813330e89fcc2
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-03 14:00:55 +0530

    feat: Start on Exercise 11

--- Chapter3/Exercise11.py
+++ Chapter3/Exercise11.py
@@ -29,11 +29,18 @@
 # %%
 import numpy as np
 import pandas as pd
-N = 100
-rng = np.random.default_rng(1)
-x = rng.normal(size=N)
-y = 2 * x + rng.normal(size=N)
-df = pd.DataFrame({"x": x,"y":y})
+
+def generate_data(mean=0.0, sd=1.0):
+  N = 100
+  rng = np.random.default_rng(1)
+  x = rng.normal(size=N)
+  y = 2 * x + rng.normal(loc=mean,scale=sd, size=N)
+  df = pd.DataFrame({"x": x,"y":y})
+  return df
+
+df = generate_data()
+std_x = np.std(df["x"])
+std_y = np.std(df["y"])
 df.head()
 
 
@@ -79,7 +86,29 @@ print(f"{results.params/results.bse}")
 # %% [markdown]
 # ## (c) What is the relationship between the results obtained in (a) and (b)?
 
+# %% [markdown]
+# ### Why should the estimates of the coefficient of $\beta_{1}$ differ from the expected value of 0.5 in the regression of $x \thicksim y + 0$?
+#
+# - The obvious answer is that the cause of the variation is because of the introduction of the error term (or residuals) that we introduced while generating y from x with a mean of 0 and a standard deviation of 1.
+# - For x and y to be perfectly symmetrical, the standard deviation of the error terms would have to be zero.
+# - We can check that with the regression below where we generate y with zero SD and regress y on x and then x on y.
+
 # %%
+df = generate_data(sd=0)
+
+# %%
+formula = "y ~ x + 0"
+model = smf.ols(f'{formula}', df)
+results = model.fit()
+result_df = pd.DataFrame({"coefficient": results.params, "se": results.bse, "tstatistic": results.tvalues, "p-value":results.pvalues})
+result_df
+
+# %%
+formula = "x ~ y + 0"
+model = smf.ols(f'{formula}', df)
+results = model.fit()
+result_df = pd.DataFrame({"coefficient": results.params, "se": results.bse, "tstatistic": results.tvalues, "p-value":results.pvalues})
+result_df
 
 # %% [markdown]
 # ## (d) For the regression of Y onto X without an intercept, the t-statistic for $H_{0} : \beta = 0$ takes the form $\hat{\beta} / SE(\hat{\beta})$, where $\hat{\beta}$ is given by (3.38), and where
