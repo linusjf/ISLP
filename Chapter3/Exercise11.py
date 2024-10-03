821c4f29e1cb985d5c1d7f718b132d821d85b46c
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-03 16:05:40 +0530

    feat: Start on Exercise 11

--- Chapter3/Exercise11.py
+++ Chapter3/Exercise11.py
@@ -16,6 +16,12 @@
 # # Exercise 11
 
 # %% [markdown]
+# ## Import notebook funcs
+
+# %%
+from notebookfuncs import *
+
+# %% [markdown]
 # ## In this problem we will investigate the t-statistic for the null hypothesis $H_{0} : \beta = 0$ in simple linear regression without an intercept. To begin, we generate a predictor x and a response y as follows.
 #
 # ```python
@@ -49,10 +55,21 @@ df.head()
 
 # %%
 import statsmodels.formula.api as smf
+
+def get_results_df(results):
+  result_df = pd.DataFrame({"coefficient": results.params,
+                          "se": results.bse,
+                          "tstatistic": results.tvalues,
+                          "p-value":results.pvalues,
+                         "r-squared": results.rsquared,
+                         "pearson_coefficient": np.sqrt(results.rsquared)
+                           })
+  return result_df
+
 formula = "y ~ x + 0"
 model = smf.ols(f'{formula}', df)
 results = model.fit()
-result_df = pd.DataFrame({"coefficient": results.params, "se": results.bse, "tstatistic": results.tvalues, "p-value":results.pvalues})
+result_df = get_results_df(results)
 result_df
 
 # %%
@@ -71,7 +88,7 @@ print(f"{results.params/results.bse}")
 formula = "x ~ y + 0"
 model = smf.ols(f'{formula}', df)
 results = model.fit()
-result_df = pd.DataFrame({"coefficient": results.params, "se": results.bse, "tstatistic": results.tvalues, "p-value":results.pvalues})
+result_df = get_results_df(results)
 result_df
 
 # %%
@@ -82,6 +99,10 @@ print(f"{results.params/results.bse}")
 # - The $\beta_{1}$ estimate, in this case, is 0.375744 which is not as close as one would expect to 0.5
 # - The p-value of 6.231546$e^{-31}$ signifies a strong relationship between x and y of the form $x = 0.375744 * y$
 # - The t-statistics and, hence, the p-values are identical in both regressions.
+# - We can also see that the $R^{2}$ and $\rho$ (Pearson coefficient) are identical in both regressions.
+
+# %%
+pearson_coefficient = result_df["pearson_coefficient"]
 
 # %% [markdown]
 # ## (c) What is the relationship between the results obtained in (a) and (b)?
@@ -94,7 +115,10 @@ print(f"{results.params/results.bse}")
 # - We can check that with the regression below where we generate y with zero SD and regress y on x and then x on y.
 
 # %%
-df = generate_data(sd=0)
+df = generate_data(sd=0);
+std_x_perfect = np.std(df["x"])
+std_y_perfect = np.std(df["y"])
+std_x_perfect,std_y_perfect
 
 # %%
 formula = "y ~ x + 0"
@@ -109,6 +133,28 @@ model = smf.ols(f'{formula}', df)
 results = model.fit()
 result_df = pd.DataFrame({"coefficient": results.params, "se": results.bse, "tstatistic": results.tvalues, "p-value":results.pvalues})
 result_df
+
+# %% [markdown]
+# ### Now, the formula for $\beta_{1(y \: on\: x)}$ is $\rho * \large \frac {S_{y}} {S_{x}}$ and for $\beta_{1(x\: on \: y)}$ is $\rho * \large \frac {S_{x}} {S_{y}}$.
+
+# %% [markdown]
+# ### Using the formulae above:
+
+# %%
+beta_1_x_on_y = pearson_coefficient * std_x / std_y
+beta_1_y_on_x = pearson_coefficient * std_y / std_x
+beta_1_y_on_x, beta_1_x_on_y
+
+# %% [markdown]
+# ### Now, if Pearson coeffcient were 1.0, i.e., there is perfect correlation between x and y, then we can calculate the betas and find them to be reciprocals of each other.
+
+# %%
+beta_1_x_on_y = 1.0 * std_x_perfect / std_y_perfect
+beta_1_y_on_x = 1.0 * std_y_perfect / std_x_perfect
+beta_1_y_on_x, beta_1_x_on_y
+
+# %% [markdown]
+# - Reference: <https://stats.stackexchange.com/questions/22718/what-is-the-difference-between-linear-regression-on-y-with-x-and-x-with-y>
 
 # %% [markdown]
 # ## (d) For the regression of Y onto X without an intercept, the t-statistic for $H_{0} : \beta = 0$ takes the form $\hat{\beta} / SE(\hat{\beta})$, where $\hat{\beta}$ is given by (3.38), and where
@@ -140,3 +186,4 @@ result_df
 # ## (f) In Python, show that when regression is performed with an intercept, the t-statistic for $H_{0} : \beta_{1} = 0$ is the same for the regression of y onto x as it is for the regression of x onto y.
 
 # %%
+allDone();
