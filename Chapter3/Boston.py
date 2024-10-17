91775c29109be377709a88c4128a1c426229e956
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-17 12:26:50 +0530

    feat: start exercise 14.

--- Chapter3/Boston.py
+++ Chapter3/Boston.py
@@ -13,13 +13,19 @@
 # ---
 
 # %% [markdown]
-# # Exercise 15: This problem involves the Boston data set, which we saw in the lab for this chapter. 
+# # Exercise 15: This problem involves the Boston data set, which we saw in the lab for this chapter.
 
 # %% [markdown]
 # ## Import notebook funcs
 
 # %%
 from notebookfuncs import *
+
+# %% [markdown]
+# ## Import userfuncs
+
+# %%
+from userfuncs import *
 
 # %% [markdown]
 # ## Import libraries
@@ -33,19 +39,21 @@ Boston = load_data("Boston")
 dfSummary(Boston)
 
 # %% [markdown]
-# ## We will now try to predict per capita crime rate using the other variables in this data set. 
+# ## We will now try to predict per capita crime rate using the other variables in this data set.
 
 # %% [markdown]
-# ## In other words, per capita crime rate is the response, and the other variables are the predictors.
+# ## In other words, per capita crime rate (crim) is the response, and the other variables are the predictors.
 
 # %% [markdown]
 # ## (a) For each predictor, fit a simple linear regression model to predict the response.
+
+# %%
 
 # %% [markdown]
 # ### Describe your results.
 
 # %% [markdown]
-#  
+#
 # ### In which of the models is there a statistically significant association between the predictor and the response?
 
 # %% [markdown]
@@ -55,22 +63,22 @@ dfSummary(Boston)
 # ## (b) Fit a multiple regression model to predict the response using all of the predictors.
 
 # %% [markdown]
-# ## Describe your results. 
+# ## Describe your results.
 
 # %% [markdown]
-# ## For which predictors can we reject the null hypothesis H0 : βj = 0?
+# ## For which predictors can we reject the null hypothesis $H_0 : \beta_j = 0$?
 
 # %% [markdown]
-# ## (c) How do your results from (a) compare to your results from (b)? 
+# ## (c) How do your results from (a) compare to your results from (b)?
 
 # %% [markdown]
 # ### Create a plot displaying the univariate regression coefficients from (a) on the x-axis, and the multiple regression coefficients from (b) on the y-axis. That is, each predictor is displayed as a single point in the plot. Its coefficient in a simple linear regression model is shown on the x-axis, and its coefficient estimate in the multiple linear regression model is shown on the y-axis.
 
 # %% [markdown]
-# ## (d) Is there evidence of non-linear association between any of the predictors and the response? 
+# ## (d) Is there evidence of non-linear association between any of the predictors and the response?
 
 # %% [markdown]
-# ### To answer this question, for each predictor X, fit a model of the form Y = β0 + β1 X + β2 X <sup>2</sup> + β3 X <sup>3</sup> + $\epsilon$
+# ### To answer this question, for each predictor X, fit a model of the form $Y = \beta_0 + \beta_1 * X + \beta_2 * X^2 + \beta_3 * X^3 + \epsilon$
 
 # %%
 allDone();
