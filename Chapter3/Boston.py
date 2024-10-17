5507d6973bd9cf7a22aa0e326f6c34b0420143ac
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-17 09:55:01 +0530

    feat: start exercise 14.

--- Chapter3/Boston.py
+++ Chapter3/Boston.py
@@ -22,22 +22,55 @@
 from notebookfuncs import *
 
 # %% [markdown]
+# ## Import libraries
+
+# %%
+from ISLP import load_data
+from summarytools import dfSummary
+
+# %%
+Boston = load_data("Boston")
+dfSummary(Boston)
+
+# %% [markdown]
 # ## We will now try to predict per capita crime rate using the other variables in this data set. 
 
 # %% [markdown]
 # ## In other words, per capita crime rate is the response, and the other variables are the predictors.
 
 # %% [markdown]
-# ## (a) For the response. Describe your results. In which of the models is there a statistically significant association between the predictor and the response? Create some plots to back up your assertions.
+# ## (a) For each predictor, fit a simple linear regression model to predict the response.
 
 # %% [markdown]
-# ## (b) Fit a multiple regression model to predict the response using all of the predictors. Describe your results. For which predictors can we reject the null hypothesis H0 : βj = 0?
+# ### Describe your results.
 
 # %% [markdown]
-# ## (c) How do your results from (a) compare to your results from (b)? Create a plot displaying the univariate regression coefficients from (a) on the x-axis, and the multiple regression coefficients from (b) on the y-axis. That is, each predictor is displayed as a single point in the plot. Its coefficient in a simple linear regression model is shown on the x-axis, and its coefficient estimate in the multiple linear regression model is shown on the y-axis.
+#  
+# ### In which of the models is there a statistically significant association between the predictor and the response?
 
 # %% [markdown]
-# ## (d) Is there evidence of non-linear association between any of the predictors and the response? To answer this question, for each predictor X, fit a model of the form Y = β0 + β1 X + β2 X <sup>2</sup> + β3 X <sup>3</sup> + $\epsilon$
+# ### Create some plots to back up your assertions.
+
+# %% [markdown]
+# ## (b) Fit a multiple regression model to predict the response using all of the predictors.
+
+# %% [markdown]
+# ## Describe your results. 
+
+# %% [markdown]
+# ## For which predictors can we reject the null hypothesis H0 : βj = 0?
+
+# %% [markdown]
+# ## (c) How do your results from (a) compare to your results from (b)? 
+
+# %% [markdown]
+# ### Create a plot displaying the univariate regression coefficients from (a) on the x-axis, and the multiple regression coefficients from (b) on the y-axis. That is, each predictor is displayed as a single point in the plot. Its coefficient in a simple linear regression model is shown on the x-axis, and its coefficient estimate in the multiple linear regression model is shown on the y-axis.
+
+# %% [markdown]
+# ## (d) Is there evidence of non-linear association between any of the predictors and the response? 
+
+# %% [markdown]
+# ### To answer this question, for each predictor X, fit a model of the form Y = β0 + β1 X + β2 X <sup>2</sup> + β3 X <sup>3</sup> + $\epsilon$
 
 # %%
 allDone();
