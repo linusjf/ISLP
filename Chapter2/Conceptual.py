5c07b05972a99b323ca1b2f27ee1f87930840624
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-20 17:59:58 +0530

    feat: start conceptual exercises for Chapter 2

--- Chapter2/Conceptual.py
+++ Chapter2/Conceptual.py
@@ -59,7 +59,8 @@ def allDone():
 # %% [markdown]
 # ### (d) The variance of the error terms, i.e.. $\sigma^2 = Var(\epsilon)$, is extremely high.
 
-# %%
+# %% [markdown]
+# - When the variance of the error terms is very high, the flexible model is worse since it will tend to overfit the model i.e.. it will tend to explain part of the variance as well i.e, it will tend to fit too much of the noise.
 
 # %% [markdown]
 # ## 2. Explain whether each scenario is a classification or regression problem, and indicate whether we are most interested in inference or prediction. Finally, provide n and p.
@@ -68,10 +69,28 @@ def allDone():
 # ### (a) We collect a set of data on the top 500 firms in the US. For each firm we record profit, number of employees, industry and the CEO salary. We are interested in understanding which factors affect CEO salary.
 
 # %% [markdown]
+# - The CEO Salary is a continuous variable.
+# - n = 500
+# - p = 3
+# - The problem is thus a regression problem.
+
+# %% [markdown]
 # ### (b) We are considering launching a new product and wish to know whether it will be a success or a failure. We collect data on 20 similar products that were previously launched. For each product we have recorded whether it was a success or failure, price charged for the product, marketing budget, competition price, and ten other variables.
 
 # %% [markdown]
+# - Response variable is Success/ Failure of the new product
+# - n = 20
+# - p = 13
+# - The problem is a classification one.
+
+# %% [markdown]
 # ### (c) We are interested in predicting the % change in the USD/Euro exchange rate in relation to the weekly changes in the world stock markets. Hence we collect weekly data for all of 2012. For each week we record the % change in the USD/Euro, the %change in the US market, the % change in the British market, and the % change in the German market.
+
+# %% [markdown]
+# - Response is %change in USD/Euro exchange rate. This is a continuous variable.
+# - n = 52
+# - p = 3
+# - The problem is thus a regression problem.
 
 # %% [markdown]
 # ## 3. We now revisit the bias-variance decomposition.
