92436bd640e0d985acababb8758f081ead4f9285
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-23 09:53:58 +0530

    feat: Start Chapter 4 lab

--- Chapter4/Labs.py
+++ Chapter4/Labs.py
@@ -22,7 +22,7 @@
 from notebookfuncs import *
 
 # %% [markdown]
-# ## Examine the Smarket data --- part of the ISLP library.
+# ## Examine the Smarket data &mdash; part of the ISLP library.
 
 # %% [markdown]
 # ### Consists of percentage returns for the S&P 500  stock index over 1,250 days, from the beginning of 2001 until the end of 2005.
@@ -75,6 +75,48 @@ Smarket.corr(numeric_only=True)
 
 # %%
 Smarket.plot(y='Volume');
+
+# %% [markdown]
+# ## Logistic Regression
+
+# %% [markdown]
+# ### Fit a logistic regression model to predict Direction using Lag1 through Lag5 and Volume. 
+
+# %% [markdown]
+# We use the sm.GLM() function which fits Generalized Linear Models (GLMs) which includes logistic regression. We could alos sm.Logit() which fits a logit model directly.
+# The syntax of sm.GLM() is similar to that of sm.OLS(), except that we must pass in the argument family=sm.families.Binomial() in order to tell statsmodels to run a logistic regression rather than some other type of GLM.
+
+# %%
+allvars = Smarket.columns.drop(['Today', 'Direction', 'Year'])
+design = MS(allvars)
+X = design.fit_transform(Smarket)
+y = Smarket.Direction == 'Up'
+family = sm.families.Binomial()
+glm = sm.GLM(y, X, family=family)
+results = glm.fit()
+summarize(results)
+
+# %%
+(results.pvalues.idxmin(), results.pvalues.min())
+
+# %% [markdown]
+# - The smallest p-value here is associated with Lag1. 
+# - The negative coefficient for this predictor suggests that if the market had a positive return yesterday then it is less likely to go up today.
+# - However, at a value of 0.15, the p-value is still relatively large.
+# - So there is no clear evidence of a real association between Lag1 and Direction.
+
+# %%
+results.params
+
+# %% [markdown]
+# ### Predict 
+
+# %% [markdown]
+# The predict() method of results can be used to predict the probability that the market will go up, given values of the predictors. This method returns predictions on the probability scale. If no data set is supplied to the predict() function, then the probabilities are computed for the training data that was used to fit the logistic regression model. As with linear regression, one can pass an optional exog argument consistent with a design matrix if desired. Here we have printed only the first ten probabilities.
+
+# %%
+probs = results.predict()
+probs[:10]
 
 # %%
 allDone();
