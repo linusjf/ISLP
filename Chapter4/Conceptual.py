8bec32ec56ee92bad8dd7dc4415a213fb407a2e3
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-25 10:53:26 +0530

    feat: Start setup of Conceptual exercises.

--- Chapter4/Conceptual.py
+++ Chapter4/Conceptual.py
@@ -55,7 +55,8 @@ from notebookfuncs import *
 # $$
 #
 # (4.16). Prove that in this case, the Bayes classifier is not linear. Argue that it is in fact quadratic. 
-# *Hint: For this problem, you should follow the arguments laid out in Section 4.4.1, but without making the assumption that $\sigma_1^2 = · · · = \sigma_K^2$*
+#
+# **Hint: For this problem, you should follow the arguments laid out in Section 4.4.1, but without making the assumption that $\sigma_1^2 = · · · = \sigma_K^2$**
 
 # %% [markdown]
 # ## Exercise 4
@@ -64,6 +65,48 @@ from notebookfuncs import *
 # %% [markdown]
 # ### (a)
 # Suppose that we have a set of observations, each with measurements on p = 1 feature, X. We assume that X is uniformly (evenly) distributed on [0, 1]. Associated with each observation is a response value. Suppose that we wish to predict a test observation’s response using only observations that are within 10 % of the range of X closest to that test observation. For instance, in order to predict the response for a test observation with X = 0.6, we will use observations in the range [0.55, 0.65]. On average, what fraction of the available observations will we use to make the prediction?
+
+# %% [markdown]
+# ### (b)
+# Now suppose that we have a set of observations, each with measurements on p = 2 features, $X_1$ and $X_2$ . We assume that $(X_1 , X_2 )$ are uniformly distributed on [0, 1] × [0, 1]. We wish to predict a test observation’s response using only observations that are within 10 % of the range of $X_1$ and within 10 % of the range of $X_2$ closest to that test observation. For instance, in order to predict the response for a test observation with $X_1$ = 0.6 and $X_2$ = 0.35, we will use observations in the range [0.55, 0.65] for $X_1$ and in the range [0.3, 0.4] for $X_2$ . On average, what fraction of the available observations will we use to make the prediction?
+
+# %% [markdown]
+# ### (c)
+# Now suppose that we have a set of observations on p = 100 features. Again the observations are uniformly distributed on each feature, and again each feature ranges in value from 0 to 1. We wish to predict a test observation’s response using observations within the 10% of each feature’s range that is closest to that test observation. What fraction of the available observations will we use to make the prediction?
+
+# %% [markdown]
+# ### (d)
+# Using your answers to parts (a)–(c), argue that a drawback of KNN when p is large is that there are very few training observations “near” any given test observation.
+
+# %% [markdown]
+# ### (e)
+# Now suppose that we wish to make a prediction for a test observation by creating a p-dimensional hypercusegment, when p = 2 it is a square, and when p = 100 it is abe centered around the test observation that contains, on average, 10 % of the training observations. For p = 1, 2, and 100, what is the length of each side of the hypercube? Comment on your answer.
+#
+# **Note: A hypercube is a generalization of a cube to an arbitrary number of dimensions. When p = 1, a hypercube is simply a line segment, when p = 2 it is a square, and when p = 100 it is a 100-dimensional cube.**
+
+# %% [markdown]
+# ## Exercise 5
+
+# %% [markdown]
+# ## Exercise 6
+
+# %% [markdown]
+# ## Exercise 7
+
+# %% [markdown]
+# ## Exercise 8
+
+# %% [markdown]
+# ## Exercise 9
+
+# %% [markdown]
+# ## Exercise 10
+
+# %% [markdown]
+# ## Exercise 11
+
+# %% [markdown]
+# ## Exercise 12
 
 # %%
 allDone();
