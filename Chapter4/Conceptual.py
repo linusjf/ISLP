15652121dce17d9a32a01007e4d0057d23883d2b
Linus Fernandes <linus.fernandes@gmail.com>
2024-10-25 10:29:35 +0530

    feat: Start setup of Conceptual exercises.

--- Chapter4/Conceptual.py
+++ Chapter4/Conceptual.py
@@ -47,7 +47,23 @@ from notebookfuncs import *
 # $$
 # (4.18) is the largest. Prove that this is the case. In other words, under the assumption that the observations in the $k_{th}$ class are drawn from a $N(\mu_k , \sigma^2)$ distribution, the Bayes classifier assigns an observation to the class for which the discriminant function is maximized.
 
-# %%
+# %% [markdown]
+# ## Exercise 3 
+# This problem relates to the QDA model, in which the observations within each class are drawn from a normal distribution with a class specific mean vector and a class specific covariance matrix. We consider the simple case where p = 1; i.e. there is only one feature. Suppose that we have K classes, and that if an observation belongs to the kth class then X comes from a one-dimensional normal distribution, $X ∼ N(\mu_k , \sigma_k^2)$. Recall that the density function for the one-dimensional normal distribution is given in 
+# $$
+# \huge f_k(x) = \frac {1} {\sqrt{2\pi}\sigma_k} exp(- \frac {1} {2\sigma_k^2}(x - \mu_k)^2)
+# $$
+#
+# (4.16). Prove that in this case, the Bayes classifier is not linear. Argue that it is in fact quadratic. 
+# *Hint: For this problem, you should follow the arguments laid out in Section 4.4.1, but without making the assumption that $\sigma_1^2 = · · · = \sigma_K^2$*
+
+# %% [markdown]
+# ## Exercise 4
+# When the number of features p is large, there tends to be a deterioration in the performance of KNN and other local approaches that perform prediction using only observations that are near the test observation for which a prediction must be made. This phenomenon is known as the curse of dimensionality, and it ties into the fact that non-parametric approaches often perform poorly when p is large. We will now investigate this curse.
+
+# %% [markdown]
+# ### (a)
+# Suppose that we have a set of observations, each with measurements on p = 1 feature, X. We assume that X is uniformly (evenly) distributed on [0, 1]. Associated with each observation is a response value. Suppose that we wish to predict a test observation’s response using only observations that are within 10 % of the range of X closest to that test observation. For instance, in order to predict the response for a test observation with X = 0.6, we will use observations in the range [0.55, 0.65]. On average, what fraction of the available observations will we use to make the prediction?
 
 # %%
 allDone();
