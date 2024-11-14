cb020bd1d9c11f8233c280edee4051f4dc4c5e48
Linus Fernandes <linus.fernandes@gmail.com>
2024-11-14 17:29:47 +0530

    feat: Solve conceptual exercise 2 for Chapter 5.

--- Chapter5/Conceptual.py
+++ Chapter5/Conceptual.py
@@ -84,30 +84,74 @@ from notebookfuncs import *
 # ### (d) 
 # **When n = 5, what is the probability that the $j_{th}$ observation is in the bootstrap sample?**
 
+# %%
+import sympy
+from sympy import Symbol, N
+n = Symbol('n')
+prob_j = 1 - (1 - 1 / n) ** n
+N(prob_j.subs(n, 5))
+
 # %% [markdown]
 # ### (e) 
 # **When n = 100, what is the probability that the $j_{th}$ observation is in the bootstrap sample?**
+
+# %%
+N(prob_j.subs(n, 100))
 
 # %% [markdown]
 # ### (f)
 # **When n = 10, 000, what is the probability that the $j_{th}$ observation is in the bootstrap sample?**
 
+# %%
+N(prob_j.subs(n, 10000))
+
 # %% [markdown]
 # ### (g) 
 # **Create a plot that displays, for each integer value of n from 1 to 100, 000, the probability that the $j_{th}$ observation is in the bootstrap sample. Comment on what you observe.**
+
+# %%
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.stats import mode
+probs = np.zeros(100000)
+for i in range(100000):
+  n = i + 1
+  probs[i] = 1 - (1 - 1 / n) ** n
+plt.title("Probability that the $j_{th}$ observation is in the bootstrap sanple.")
+plt.xlabel("Sample size")
+plt.ylabel("Probability");
+plt.plot(probs);
+
+# %%
+printmd("Here, we can see that as the size of the bootstrap sample increases, the probability of selecting the $j_{th}$ samples stabilizes at ")
+printmd(f"**{mode(probs)[0]:.4f}**.")
 
 # %% [markdown]
 # ### (h)
 # **We will now investigate numerically the probability that a bootstrap sample of size n = 100 contains the $j_{th}$ observation. Here j = 4. We first create an array store with values that will subsequently be overwritten using the function `np.empty()`. We then repeatedly create bootstrap samples, and each time we record whether or not the fifth observation is contained in the bootstrap sample.**
 #
 # ```python
-# rng = np.random.default_rng (10)
-# store = np.empty (10000)
-# for i in range (10000):
-#   store[i] = np.sum(rng.choice (100, replace=True) == 4) > 0
+# rng = np.random.default_rng(10)
+# store = np.empty(10000)
+# for i in range(10000):
+#   store[i] = np.sum(rng.choice(100, size=100, replace=True) == 4) > 0
 # np.mean(store)
 # ```
 # **Comment on the results obtained.**
+
+# %%
+rng = np.random.default_rng(10)
+store = np.empty(10000)
+for i in range(10000):
+  # This stores the number of bootstrap samples that contain 4
+  store[i] = np.sum(rng.choice(100, size=100, replace=True) == 4) > 0
+np.mean(store)
+
+# %% [markdown]
+# *Note: We had to change the code above so that 100 numbers are sampled from the range 0 - 99 and thus 10000 bootstrap samples are generated. The earlier code only sampled 1 element at a time.*
+
+# %%
+printmd(f"Thus, we see that the probability closely matches what we obtained theoretically at {np.mean(store)}.")
 
 # %% [markdown]
 # ## Exercise 3
