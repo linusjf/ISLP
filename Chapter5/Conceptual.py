c783c1e92fbfc3295ac35749808c8a319fb7a773
Linus Fernandes <linus.fernandes@gmail.com>
2024-11-15 11:53:13 +0530

    feat: Solve conceptual exercise 2 for Chapter 5.

--- Chapter5/Conceptual.py
+++ Chapter5/Conceptual.py
@@ -71,13 +71,19 @@ from notebookfuncs import *
 # **What is the probability that the second bootstrap observation is not the $j_{th}$ observation from the original sample?**
 
 # %% [markdown]
-# We are sampling with replacement since bootstrap implies the same, the probability that the second observation is not the $j_{th}$ observation is $(1 - \frac{1} {n})^2$.
+# We are sampling with replacement since bootstrap implies the same, the probability that the second observation is not the $j_{th}$ observation is $(1 - \frac{1} {n})$ as well.
 
 # %% [markdown]
 # ### (c) 
 # **Argue that the probability that the $j_{th}$ observation is not in the bootstrap sample is $(1 − \frac {1} {n})^n$.**
 
 # %% [markdown]
+# Sampling using bootstrap method involves sampling with replacement from a dataset with n items. Each observation has probability of 1/n of being selected at each draw and thus (1 - 1/n) probability of not being chosen at each draw. Since each draw is independent of each other, the probability that a select observation is not selected in the entire bootstrap sample is the product of their individual draw probabilities and is equivalent to the following:
+# $$\begin{aligned}
+# & \prod_{i=1}^n (1 - \frac {1} {n}) \\
+# & = (1 − \frac {1} {n})^n
+# \end{aligned}$$
+#
 # Thus, the probability that the $j_{th}$ observation is not in the entire bootstrap sample of size n is $(1 − \frac {1} {n})^n$.
 
 # %% [markdown]
@@ -109,22 +115,68 @@ N(prob_j.subs(n, 10000))
 # ### (g) 
 # **Create a plot that displays, for each integer value of n from 1 to 100, 000, the probability that the $j_{th}$ observation is in the bootstrap sample. Comment on what you observe.**
 
+# %% [markdown]
+# I've drawn the x-axis as log-scale since the curve appears to be just two lines, one vertical and the other horizontal over the linear values. I've also drawn the 0.632 bootstrap limit as $(1 - \frac{1}{e})$.
+
+# %% [markdown]
+# We can prove that 
+# $\lim_{n \to \infty} (1 - 1/n)^n = 1/e$
+#
+# $$\begin{aligned} 
+# \text{Let }y = (1 - 1/n)^n \\
+# \implies \ln{y} = n \ln{(1 - 1/n)} \\
+# \implies \lim_{n \to \infty} y = \lim_{n \to \infty} n \ln{(1 - 1/n)} \\
+# = \lim_{n \to \infty} \frac {\ln{(1 - 1/n)}} {(1/n)} \\
+# \end{aligned}$$
+#
+# Now, by L'Hospital's rule that 
+# $$
+# \lim_{n \to \infty} \frac {f(x)} {g(x)} = \lim_{n \to \infty} \frac {{f^{'}}(x)} {{g^{'}}(x)}
+# $$
+# We have
+# $$\begin{aligned}
+# \frac {d} {dn} \Big ( \ln{(1 - 1/n)} \Big ) \\
+# = \frac {1} { 1 - 1/n} (1 / n^2)
+# \end{aligned}$$
+# And 
+# $$\begin{aligned}
+# \frac {d} {dn} \Big ( 1/n \Big ) \\
+# =  - (1 / n^2)
+# \end{aligned}$$
+# Substituting and canceling, we have
+# $$\begin{aligned}
+# \lim_{n \to \infty} (-1) \frac {n} {n-1} \\
+# \text{Since } \lim_{n \to \infty} \frac  {n} {n-1} \text{ tends to 1 } \\
+# \implies \lim_{n \to \infty} \ln{y} = (-1) (1) = -1 \\
+# \implies y = e^{-1} = 1 / e
+# \end{aligned}$$
+#
+# Thus, the bootstrap probability limit tends to $1 - (1 - 1/n)^n \approx 1 - (1/e)$.
+
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.stats import mode
+from math import e
 probs = np.zeros(100000)
 for i in range(100000):
   n = i + 1
   probs[i] = 1 - (1 - 1 / n) ** n
 plt.title("Probability that the $j_{th}$ observation is in the bootstrap sanple.")
-plt.xlabel("Sample size")
+plt.xlabel("Log Sample size")
 plt.ylabel("Probability");
-plt.plot(probs);
+plt.gca().set_xscale("log")
+plt.gca().set_xlim((1, 100000))
+plt.gca().axhline(1 - 1/e, linestyle='--', label="Bootstrap limit $1 - \\frac{1} {e}$", c="r")
+plt.plot(probs, label="Probabilities")
+plt.legend();
 
 # %%
 printmd("Here, we can see that as the size of the bootstrap sample increases, the probability of selecting the $j_{th}$ samples stabilizes at ")
 printmd(f"**{mode(probs)[0]:.4f}**.")
+
+# %% [markdown]
+# This limit of 0.632 is also termed as the ['0.632 bootstrap rule'](https://stats.stackexchange.com/questions/96739/what-is-the-632-rule-in-bootstrapping).
 
 # %% [markdown]
 # ### (h)
